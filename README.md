cf762b4f2456796da809841569660901d3f3d1ff
Stephen Larsen <sdlarsen@ualberta.ca>
2019-04-02 23:40:21 -0600

    added indexing, untested

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # nlp_stuff
 Practicing NLP stuff, mostly just for fun
+
+Attempting to build a program which creates a language model based off Shakespeare's Sonnets, then outputs a pseudo-randomly generated sonnet based off that model. Glhf.
--- create_index.py
+++ create_index.py
@@ -6,8 +6,8 @@ from nltk.stem.porter import *
 
 '''
 Create and fill the database
-TODO -- Parse file line by line
-TODO -- FIgure out how to tokenize each word
+TODO -- Stemming has been commented out for now
+TODO -- Find way to get before/after IDs
 '''
 
 def init_db(c):
@@ -37,19 +37,84 @@ def init_db(c):
         c.execute('''
             CREATE UNIQUE INDEX Token_idx ON Token(token);
         ''')
+
     except:
         # cleanup
         c.execute("DROP TABLE IF EXISTS Token;")
         c.execute("DROP TABLE IF EXISTS Posting;")
         c.execute("DROP INDEX IF EXISTS Token_idx;")
 
-        sys.exit("Error setting up db, exiting.")
+        sys.exit("Error setting up db, aborting.")
+
+
+def parse_line(line):
+    tokens = nltk.word_tokenize(line)
+    # stemmer = PorterStemmer()
+    stemmed_tokens = []
+    punctuation = [',', '.', ';', ':', "'", '"', '-']
+
+    for token in tokens:
+        if token in punctuation:
+            continue
+
+        # token = stemmer.stem(token.lower())
+        stemmed_tokens.append(token)
+
+    return stemmed_tokens
+
+
+def index_tokens(c, tokens, id, offset):
+    before = after = None
+    for token in tokens:
+        token_id = in_index(token, c)       # check if token exists
+
+        if token_id is None:                # else, add it
+            token_id = get_highest_id(c)
+
+            if token_id is None:
+                token_id = 0
+            else:
+                token_id += 1
+
+            c.execute('''
+            INSERT INTO Token VALUES (?,?);
+            ''', (token, token_id,))
+
+        c.execute('''
+        INSERT INTO Posting VALUES (?,?,?,?,?);
+        ''', (token_id, doc_id, offset, before, after))
+
+        offset += 1
+
+    return offset
+
+
+def in_index(token, c):
+    c.execute('''
+        SELECT token_id
+        FROM Token
+        WHERE token=?;''', (token,))
+
+    token_id = c.fetchone()
+
+    if token_id is not None:
+        return token_id[0]
+
+    return None
+
+
+def get_highest_id(c):
+    c.execute('''SELECT MAX(token_id) FROM Token;''')
+    max_id = c.fetchone()
+
+    return max_id[0]
+
 
 def main():
 
     try:
         fname = sys.argv[1]
-        fh = open(fname, 'r')
+        infile = open(fname, 'r')
     except FileNotFoundError:
         sys.exit("Error, file not found.")
 
@@ -58,12 +123,23 @@ def main():
     c = conn.cursor()
     init_db(c)
 
+    id = 0
     x = 0
-    for line in fh:
-        if x == 5:
-            break
-        print(line)
-        x += 1
+    for line in infile:
+
+        line = line.strip()
+
+        try:                    # new sonnet
+            id = int(line)
+            offset = 0          # reset offset
+            # print(id)
+
+        except ValueError:                 # sonnet body
+            tokens = parse_line(line)
+            # if x < 5:
+            #     print(tokens)
+            #     x += 1
+            offset = index_tokens(c, tokens, id, offset)
 
 
 if __name__ == '__main__':
